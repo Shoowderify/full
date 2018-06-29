9d2daff83f6650bffb3132f12517df1f053817f7
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-06-29 18:45:15 -0400

    Update index.js

--- episode-18-code/index.js
+++ episode-18-code/index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
+
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client();
@@ -122,4 +122,4 @@ bot.on("message", async message => {
 
 });
 
-bot.login(tokenfile.token);
+bot.login(NDYyMzcwNDc5MDY0MjE5NjQ4.Dhg3lg.RS5tk3VCB_TmEU1tP8CD-sSY8JA);
